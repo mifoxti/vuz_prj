140fcc091819aae883f3ae738fe6f2697532f608
mifoxti <microfoxti@gmail.com>
2023-11-23 13:15:58 +0300

    Фикс 20-ой

--- vuzproject/src/upd_21/Solution.java
+++ vuzproject/src/upd_21/Solution.java
@@ -22,22 +22,25 @@ public class Solution<T, V, K> {
     }
 
     public static void main(String[] args) {
-        Integer[] intArray = {1, 2, 3, 4, 5};
-        List<Integer> intList = arrayToList(intArray);
-
-        System.out.println("List from Integer array: " + intList);
-
-        String[] stringArray = {"apple", "banana", "cherry"};
-        List<String> stringList = arrayToList(stringArray);
-
-        System.out.println("List from String array: " + stringList);
-
-        File[] filesInDirectory = new File("src").listFiles();
-        List<File> fileList = arrayToList(filesInDirectory);
-
-        System.out.println("List of files in directory: " + fileList);
-
+        // Создание экземпляра обобщенного класса Solution с типами Integer, String и Double
         Solution<Integer, String, Double> solution = new Solution<>(new Integer[]{1, 2, 3, 4, 5});
-        System.out.println("Element at index 2: " + solution.getElementAtIndex(2));
+        System.out.println("Элемент по индексу 2: " + solution.getElementAtIndex(2));
+
+        // Создание массива целых чисел
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        // Преобразование массива целых чисел в список целых чисел с использованием статического метода
+        List<Integer> intList = arrayToList(intArray);
+        System.out.println("Список из массива целых чисел: " + intList);
+
+        // Создание массива строк
+        String[] stringArray = {"Виски", "Кола", "Королева", "Танцпола"};
+        // Преобразование массива строк в список строк с использованием статического метода
+        List<String> stringList = arrayToList(stringArray);
+        System.out.println("Список из массива строк: " + stringList);
+
+        // Получение списка файлов в каталоге "vuzproject/src" и преобразование массива в список файлов
+        File[] filesInDirectory = new File("vuzproject/src").listFiles();
+        List<File> fileList = arrayToList(filesInDirectory);
+        System.out.println("Список файлов в каталоге: " + fileList);
     }
 }
